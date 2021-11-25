b682a69d103cecabe7a7de6081fd57e3fdf26e6d
ndenisj <denis.john.ndarake@gmail.com>
2021-11-25 18:05:06 +0100

    added status code

---
+++ lib/data/network/error_handler.dart
@@ -0,0 +1,36 @@
+enum DataSource {
+  SUCCESS,
+  NO_CONTENT,
+  BAD_REQUEST,
+  FORBIDDEN,
+  UNAUTHORISED,
+  NOT_FOUND,
+  INTERNAL_SERVER_ERROR,
+  UNKNOWN,
+  CONNECTION_TIMEOUT,
+  CANCEL,
+  RECEIVE_TIMEOUT,
+  SEND_TIMEOUT,
+  CACHE_ERROR,
+  NO_INTERNET_CONNECTION
+}
+
+class ResponseCode {
+  // Api status code
+  static const int SUCCESS = 200;
+  static const int NO_CONTENT = 201;
+  static const int BAD_REQUEST = 400;
+  static const int FORBIDDEN = 403;
+  static const int UNAUTHORISED = 401;
+  static const int NOT_FOUND = 404;
+  static const int INTERNAL_SERVER_ERROR = 500;
+
+  // Local status code
+  static const int UNKNOWN = -1;
+  static const int CONNECTION_TIMEOUT = -2;
+  static const int CANCEL = -3;
+  static const int RECEIVE_TIMEOUT = -4;
+  static const int SEND_TIMEOUT = -5;
+  static const int CACHE_ERROR = -6;
+  static const int NO_INTERNET_CONNECTION = -7;
+}
